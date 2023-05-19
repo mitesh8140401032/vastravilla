30efefa9d0f054744571d3695a05ad18c09b2c2d
naitiksheladiya <sheladiyanaitik502@gmail.com>
2023-05-19 17:06:43 +0530

    add

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
-
+import Button from 'react-bootstrap/Button';
+// import Offcanvas from 'react-bootstrap/Offcanvas';
 export default function Data() {
 
   return (
@@ -9,13 +9,27 @@ export default function Data() {
         <div className="row">
           <div className="aaa"></div>
           <div className="bbb">
-            <div className="d-flex justify-content-start">
-              <button className='bbb2' >
-                Launch
-              </button>
+            <div className="m">
+              <div className="d-flex justify-content-start">
+                <button class=" bbb2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">                <i class="bi bi-list"></i>
+                </button>
+
+                <div class="offcanvas offcanvas-start " data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                  <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  </div>
+                  <div class="offcanvas-body">
+                    <div className="aaa"></div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
 
+          
+          
         </div>
       </div>
     </>
